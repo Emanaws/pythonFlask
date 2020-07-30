f6578532684f0e166ccd7b33684660bba62733a8
Emanaws <redwanomer44@gmail.com>
2020-07-30 00:13:14 -0500

    Final project

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from flask import Flask,render_template
 app=Flask(__name__)
 
+Employee = {"Fname":"Redwan","Lname":"Omer","Age":32}
+
+
 @app.route('/')
 def home():
     return render_template('Home.html')
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', Employeerec= Employee )
 
 @app.route('/checkout')
 def checkout():
     return render_template('checkout.html')
+
+@app.route('/product')
+def product():
+    return render_template('product.html')
 
 
 if __name__ == "__main__":
