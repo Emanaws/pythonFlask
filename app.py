03c881beaf50976ed84dde6cd389551d3616f1ad
Emanaws <redwanomer44@gmail.com>
2020-07-29 22:05:46 -0500

    Flask2

--- app.py
+++ app.py
@@ -1,25 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 app=Flask(__name__)
-@app.route('/')
 
+@app.route('/')
 def home():
 
-    return render_template('Home.html')
-
-@app.route('/about')
-
-def About():
-
-    return render_template('about.html')
-
-@app.route('/checkout')
-def checkout():
-    return render_template('checkout.html')
-
-@app.route('/shop')
-def shop():
-    return render_template('shop.html')
-
+     return '<h1>Hello Hager!</h1>'
 
 
 if __name__ == "__main__":
